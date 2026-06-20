681f0f1a44385ab7447bb375d5f2bc01a70ef5d9
yooaknow <dbdkfma1228@naver.com>
2026-06-20 16:01:45 +0900

    feat: 기억 영수증 화면용 폰트 및 이미지 에셋 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { useFonts } from "expo-font";
 
 export default function RootLayout() {
   const [loaded] = useFonts({
+    Hana2Bold: require("../assets/fonts/Hana2-Bold.ttf"),
+    Hana2CM: require("../assets/fonts/Hana2-CM.ttf"),
     PretendardRegular: require("../assets/fonts/Pretendard-Regular.otf"),
     PretendardMedium: require("../assets/fonts/Pretendard-Medium.otf"),
     PretendardSemiBold: require("../assets/fonts/Pretendard-SemiBold.otf"),
